dc52b2db7cb996b3e7ea3286a8ad227a54c67bfb
Karl Bonnici <karl.bonnici100@gmail.com>
2022-05-12 10:18:23 -0700

    add cmd line args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use std::env;
+
 use encryption::encrypt;
 
+fn parse_args(args: &mut env::Args) -> Option<String> {
+    args.next();
+    match args.next() {
+        Some(x) => return Some(String::from(x)),
+        None => return None
+    }
+}
+
 fn main() {
-    let result = encrypt(String::from("xyz"));
-    println!("{}", result);
+    let input = parse_args(&mut env::args());
+    match input {
+        Some(i) => {
+            let result = encrypt(i);
+            println!("{}", result);
+        }
+        None => println!("usage: cargo run [input]")
+    }
 }
